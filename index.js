ad891ee13eae5d169ac0be600dfeb23df6524238
Medhanye Weldegebriel <91928287+medhanye-15@users.noreply.github.com>
2022-03-18 07:38:40 +0000

    add

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine','ejs');
+
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  var title = 'My Home Page';
+  res.render('pages/index',{'title':title});
+});
+
+app.get('/about-us', (req, res) => {
+  var title = 'My About Us Page';
+  res.render('pages/about-us',{'title':title});
+});
+app.listen(port, () =>{
+  
+  console.log(`Example app listening on port${port}`);
+
+});
