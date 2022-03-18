6c96fa6e9029a28aac9ef1bb2c1a16312e7c204c
Medhanye Weldegebriel <91928287+medhanye-15@users.noreply.github.com>
2022-03-18 09:20:51 +0000

    added data

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+//here is our test data
+var data = require('./data/test.json');
 app.set('view engine','ejs');
 
 
@@ -17,8 +18,29 @@ app.get('/about-us', (req, res) => {
   var title = 'My About Us Page';
   res.render('pages/about-us',{'title':title});
 });
-app.listen(port, () =>{
-  
-  console.log(`Example app listening on port${port}`);
 
+
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+
+});
+
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
+app.listen(port, () =>{
+  console.log(`Example app listening on port${port}`);
+  console.log(data);
 });
